9a93af337ad40053e59198374456ac8a10d61bd9
Ben Greenberg <bsgreenb@gmail.com>
2017-05-04 10:41:06 -0700

    Replace client/ directory with create-react-app

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './views/App';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
